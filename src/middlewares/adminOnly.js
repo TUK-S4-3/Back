6b84b8ecb71fde08e8891e3dd7d467b001e4395a
moot1801 <moot1801@naver.com>
2026-02-25 14:29:15 +0900

    미사용 세션 API 파일 삭제

--- src/middlewares/adminOnly.js
+++
@@ -1,9 +0,0 @@
-export function adminOnly(req, res, next) {
-  if (!req.user || req.user.role !== "ADMIN") {
-    return res.status(403).json({
-      ok: false,
-      message: "관리자 권한이 필요합니다."
-    });
-  }
-  next();
-}
